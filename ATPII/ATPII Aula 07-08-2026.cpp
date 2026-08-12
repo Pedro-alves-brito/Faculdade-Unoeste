ce44631eba4c408511feda13cb400cab17d6e3bc
Pedro-alves-brito <pedrobrito0313@gmail.com>
2026-08-11 22:17:56 -0300

    Aula 07/08/2026
    Continuação da ultima aula

--- ATPII/ATPII Aula 07-08-2026.cpp
+++ ATPII/ATPII Aula 07-08-2026.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <conio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define tfp 30
 #define tfc 10
@@ -8,11 +10,12 @@
 
 int main(void){
 	
-	int estoque[tfp], tlp=0, tlc=0, tlv=0, x, y, venda[tfv][3];
+	int estoque[tfp], tlp=0, tlc=0, tlv=0, x, y, venda[tfv][3],i;
 	float preco[tfp];
-	char produto[tfp][50], cliente[tfc][50], opcao;
+	char produto[tfp][50], cliente[tfc][50], opcao, AuxProd[50];
 	
 	do{
+		system("cls");
 		printf("\n # # #   MENU   # # #");
 		printf("\n [A] Cadastrar Produtos");
 		printf("\n [B] Relatorio de Produto");
@@ -25,18 +28,45 @@ int main(void){
 		opcao = toupper(getch());
 		
 		switch(opcao){
-			case 'A': printf("Cadastro de Produto\n");
+			case 'A': system("cls");
+				printf("Cadastro de Produto\n");
+				printf("\n Nome:");
+				fflush(stdin);
+				gets(AuxProd);
+				while(tlp<tfp && strlen(AuxProd)>0){
+					
+					strcpy(produto[tlp],AuxProd);
+					printf("Preco: R%");
+					scanf("%f",&preco[tlp]);
+					printf("Estoque: R%");
+					scanf("%d",&estoque[tlp]);
+					tlp++;
+					
+					printf("\nNome:");
+					fflush(stdin);
+					gets(AuxProd);	
+				}
 				break;
-			case 'B': printf("Relatorio de Produto\n");
+			case 'B': system("cls");
+				printf("Relatorio de Produto\n");
+				if(tlp==0)
+					printf("\nNão ha produtos!\n");
+				else{
+					for(i=0;i<tlp;i++)
+					printf("\n%s \t R$%.2f \t %d", produto[1],preco[i,estoque[i]]);
+				}
+				getch();
 				break;
-			case 'C': printf("Cadastro de Clientes\n");
+			case 'C': system("cls");
+				printf("Cadastro de Clientes\n");
 				break;
-			case 'D': printf("Relatorio de Clientes\n");
+			case 'D': system("cls");
+				printf("Relatorio de Clientes\n");
 				break;
-			case 'E': printf("Vender Produtos\n");
+			case 'E': system("cls");
+				printf("Vender Produtos\n");
 				break;
 		}
-		
 	}while(opcao != 27);
 	return 0;
 }
